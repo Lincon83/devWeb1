8fbe6b0baf3f363d031d9a3fd74be83ebc91bbf3
Lincon83 <55255589+Lincon83@users.noreply.github.com>
2020-04-16 17:45:57 -0300

    Add files via upload

---
+++ Fibonacci/fibonacci.js
@@ -0,0 +1,16 @@
+//Aula Java Script - Faesa - Prof. Otavio 
+
+var n1 = 0;
+var n2 = 1;
+var resultado = n1 + n2;
+
+console.log(n1);
+console.log(n2);
+
+for (var i=0;i<=17;i++){
+console.log(resultado);
+n1 = n2;
+n2 = resultado;
+resultado = n1 + n2;
+}
+
