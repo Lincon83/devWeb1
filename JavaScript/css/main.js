b2547d27ee4719e272d79ba5ab0e24a1ba0648dc
Lincon83 <55255589+Lincon83@users.noreply.github.com>
2020-04-18 16:15:43 -0300

    Add files via upload

---
+++ JavaScript/css/main.js
@@ -0,0 +1,14 @@
+var data = document.getElementById("data");
+var quantidade = document.getElementById("quantidade");
+
+
+var valor = document.querySelector(#valor);
+
+console.log(data.textContent);
+
+var tr = document.createElement('tr');
+
+var td = document.createElement('td');
+
+td.textContent = data.textContent;
+tr.appendChild(td);
